a016f192d599a5e87b9274bef29c3c07d81cddc6
TheMiki666 <178334893+TheMiki666@users.noreply.github.com>
2024-12-05 18:43:25 +0100

    DO my_each METHOD
    Done!

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -7,5 +7,14 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    return self unless block_given?
+    count = 0
+    while count < self.length
+      yield self[count]
+      count += 1
+    end
+
+    self
+  end
 end
